16e6f38417fd4cdc1b112f9dfa1663ef274e8e0a
Gabriel Nascimento <gbxnr25@gmail.com>
2026-03-09 21:17:37 -0300

    atualizando

---
+++ numbers.js
@@ -0,0 +1,4 @@
+var n1 = Number(window.prompt("Digite um numero:"))
+var n2 = Number(window.prompt("Digite outro numero:"))
+var S = n1 + n2
+window.alert("A soma dos valores é: " + S )
---
+++ script.js
@@ -0,0 +1,3 @@
+// variaveis
+var nome = window.prompt("QUal é o seu nome?")
+window.alert("É um grande prazer em te conhecer, " + nome)
